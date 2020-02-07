0b0e40de0fd7af0313af0b03470f507aac2d24cc
josh <jhell85@gmail.com>
2020-02-07 10:42:20 -0800

    added test that failedRed refactor

--- __tests__/GAcalculator.test.js
+++ __tests__/GAcalculator.test.js
@@ -1,5 +1,11 @@
-
+/* eslint-disable no-undef */
+import { Calculator } from "./../src/gaCalculator";
 
 describe('gaCalculator', () => {
-  
-})
+  let calculator = new Calculator(1985,6,18);
+
+  test('should return the age of the date entered', () => {
+    let birthDate = calculator.age();
+    expect(birthDate).toEqual(34);
+  });
+});
