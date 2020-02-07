42f14cfeeb6f05f4eee49a735854307627b59dc8
josh <jhell85@gmail.com>
2020-02-07 10:43:32 -0800

    made Calculator class and age constructor

---
+++ src/GAcalculator.js
@@ -0,0 +1,16 @@
+export class Calculator {
+  constructor(year, month, day){
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  age(year, month, day){
+    let today = new Date();
+    let birthDate = new Date(year, month, day);
+
+  }
+
+
+
+}
