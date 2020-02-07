4aae4fb1151ed7e62fb50809ccb9a899c6113707
josh <jhell85@gmail.com>
2020-02-07 11:08:05 -0800

    added note about tests to top of page

--- __tests__/GAcalculator.test.js
+++ __tests__/GAcalculator.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import { Calculator } from "./../src/gaCalculator";
-
+// These tests will only pass based on a date that is 34 years of age, the date would need to be changed in Calculator() 2 lines down to pass tests if the date is different then 34 years old.  
 describe('gaCalculator', () => {
   let calculator = new Calculator(1985,6,18);
 
@@ -8,4 +8,5 @@ describe('gaCalculator', () => {
     let birthDate = calculator.age();
     expect(birthDate).toEqual(34);
   });
+  
 });
