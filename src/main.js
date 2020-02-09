d43557c8bbfdafd7e4fa9cba2661a8bdf694f5f5
josh <jhell85@gmail.com>
2020-02-08 21:23:47 -0800

    added jQuery to gather stuff from form

--- src/main.js
+++ src/main.js
@@ -2,3 +2,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    // let userYear = parseInt($('input#year').val());
+    // let userMonth = parseInt($('input#month').val());
+    // let userDay = parseInt($('input#day').val());
+
+    // let userCalculator = new dayCalculator(userYear, userMonth, userDay);
+    // $('#showdayresult').html(userCalculator.checkValidDate());
+    let smoker = $("#smoker").is(":checked"); 
+    let age = parseInt($("#ageInput").val());
+    let neighborhood = $(".neighborhood:checked").val();
+    let activity = $(".activity:checked").val();
+    let 
+
+    console.log(activity);
+  
+  
+  });
+});
